0a6657d5652e462cb47bf116eb9ac94a564da350
Cvetina <cveti_mateeva@abv.bg>
2018-04-30 20:46:51 +0300

    add redux-persist to keep the state after refresh

--- client/store.js
+++ client/store.js
@@ -1,12 +1,25 @@
 import { applyMiddleware, createStore } from 'redux'
 import rootReducer from './reducers/index' 
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const middleware = applyMiddleware(thunk);
 
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    stateReconciler: autoMergeLevel2
+   };
+
+const persistReducerWrapper = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-    rootReducer, 
+    persistReducerWrapper, 
     reduxDevtools,       
     middleware
 );
+
+export const persistor = persistStore(store);
