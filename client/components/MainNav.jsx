d9e6c17263f1e5c536b637f16d2a893b1dd73d0b
Cvetina <cveti_mateeva@abv.bg>
2018-05-20 21:05:34 +0300

    fixed activClassname navLink issue

--- client/components/MainNav.jsx
+++ client/components/MainNav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { connect } from "react-redux"
-import { Link } from 'react-router-dom'
+import { connect, mapStateToProps, mapDispatchToProps } from "react-redux"
+import { NavLink, withRouter } from 'react-router-dom'
 import Home from './shared/Home'
 import Star from './shared/Star'
 import Products from './shared/Products'
@@ -17,24 +17,24 @@ class MainNav extends React.Component {
   
       return (
         <div className={style.navContainer}>
-          <Link exact activeClassName={style.current} className={style.navItem} to="/">
+          <NavLink exact activeClassName={style.current} className={style.navItem} to="/">
             <span className={style.icon}><Home /></span>
             <span className={style.text}>Home</span>
-          </Link>
-          <Link exact activeClassName={style.current} className={style.navItem} to="/products">
+          </NavLink>
+          <NavLink exact activeClassName={style.current} className={style.navItem} to="/products">
             <span className={style.icon}><Products /></span>
             <span className={style.text}>Product List</span>
-          </Link>
-          <Link exact activeClassName={style.current} className={style.navItem} to="/favourites">
+          </NavLink>
+          <NavLink exact activeClassName={style.current} className={style.navItem} to="/favourites">
             <span className={style.icon}><Star /></span>
             <span className={style.text}>Favourites</span>
             {favourites.length > 0 &&
              <span className={style.favouritesCount}>{favourites.length}</span>
             }
-          </Link>
+          </NavLink>
         </div>
       )
     }
   }
-   
-export default MainNav
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(MainNav));
